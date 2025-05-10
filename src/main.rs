95d36d84eb355147c276e41e96017c721454666b
Chin Ying <scy.6char@gmail.com>
2025-05-11 01:33:18 +0800

    feat: type / builtins
    also refactored into separate fn to handle commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const VALID_COMMANDS: [&str; 3] = ["echo", "type", "exit"];
+
+fn handle_command(command: &str) {
+    let tokens = command.split_whitespace().collect::<Vec<&str>>();
+    match command {
+        cmd if cmd.starts_with("echo") => {
+            let rest = &tokens[1..];
+            println!("{}", rest.join(" "));
+        }
+        cmd if cmd.starts_with("type") => {
+            if tokens.len() > 1 {
+                if VALID_COMMANDS.contains(&tokens[1]) {
+                    println!("{} is a shell builtin", tokens[1]);
+                } else {
+                    println!("{}: not found", tokens[1]);
+                }
+            }
+        }
+        _ => {
+            println!("{}: command not found", command);
+        }
+    }
+}
+
 fn main() {
     // by default, treat everything as invalid
     let mut exit = false;
@@ -15,15 +39,7 @@ fn main() {
         match input.trim() {
             "exit 0" => exit = true,
             command => {
-                if command.starts_with("echo") {
-                    let tokens = command.split_whitespace().collect::<Vec<&str>>();
-                    if tokens.len() > 1 {
-                        let rest = &tokens[1..];
-                        println!("{}", rest.join(" "));
-                    }
-                } else {
-                    println!("{}: command not found", input.trim());
-                }
+                handle_command(command);
             }
         }
     }
