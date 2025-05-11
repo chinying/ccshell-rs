443ac2515fa2a51d6a1d5e1449ad6ac1abb8e4d8
Chin Ying <scy.6char@gmail.com>
2025-05-11 20:06:52 +0800

    chore: use vec instead of string to represent path
    groundwork for relative paths
    currently set_current_dir("..") literally sets it to ".."

--- src/directory.rs
+++ src/directory.rs
@@ -1,14 +1,20 @@
 use std::env;
 use std::io;
 pub struct Directory {
-    current_path: String,
+    current_path: Vec<String>,
 }
 
 impl Directory {
     pub fn new() -> Self {
         Self {
             current_path: env::current_dir()
-                .map(|path| path.display().to_string())
+                .map(|path| {
+                    path.display()
+                        .to_string()
+                        .split("/")
+                        .map(|s| s.to_string())
+                        .collect()
+                })
                 .unwrap_or_default(),
         }
     }
@@ -17,13 +23,13 @@ impl Directory {
         let error_message = format!("cd: {}: No such file or directory", dir);
         env::set_current_dir(dir)
             .and_then(|_| {
-                self.current_path = dir.to_string();
+                self.current_path = dir.to_string().split("/").map(|s| s.to_string()).collect();
                 Ok(())
             })
             .map_err(|_| io::Error::new(io::ErrorKind::Other, error_message))
     }
 
     pub fn pwd(&self) -> String {
-        self.current_path.clone()
+        self.current_path.join("/")
     }
 }
