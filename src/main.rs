d701fb9cc0b2c152d934fb739aa862a1f5bdf1fd
Chin Ying <scy.6char@gmail.com>
2025-05-14 01:36:39 +0800

    feat: handle single quotes
    * adds parser to parse commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod directory;
 mod os_utils;
+mod parser;
 
 #[allow(unused_imports)]
 use std::io::{self, Write};
@@ -8,7 +9,11 @@ use std::process::Command;
 const VALID_COMMANDS: [&str; 5] = ["echo", "type", "exit", "pwd", "cd"];
 
 fn handle_command(command: &str, directory: &mut directory::Directory) -> io::Result<()> {
-    let tokens = command.split_whitespace().collect::<Vec<&str>>();
+    let parts: Vec<String> = parser::parse_command(command.to_string());
+    let tokens = parts
+        .iter()
+        .map(|s| s.as_str())
+        .collect::<Vec<&str>>();
     if tokens.is_empty() {
         return Ok(());
     }
@@ -67,8 +72,8 @@ fn handle_command(command: &str, directory: &mut directory::Directory) -> io::Re
 }
 
 fn exec(command: &str) {
-    let tokens = command.split_whitespace().collect::<Vec<&str>>();
-    let result = Command::new(tokens[0])
+    let tokens = parser::parse_command(command.to_string());
+    let result = Command::new(tokens[0].clone())
         .args(&tokens[1..])
         .spawn()
         .and_then(|mut child| child.wait());
---
+++ src/parser.rs
@@ -0,0 +1,63 @@
+fn push_str(tokens: &mut Vec<String>, str: &mut String) {
+    if !str.is_empty() {
+        tokens.push(str.clone());
+        str.clear();
+    }
+}
+
+/**
+ * collect all strings, tokenize
+ * if between quotes, don't break
+ * otherwise break on whitespaces
+ * ie. if within_quotes, continue collecting
+ * when encountering close quote, push to tokens
+ */
+pub fn parse_command(command: String) -> Vec<String> {
+    let mut tokens = Vec::new();
+    let bytes = command.as_bytes();
+    let mut within_quotes = false;
+    let mut idx = 0;
+    let mut sb = String::new();
+    while idx < command.len() {
+        // print!("{} ", bytes[idx] as char);
+        match bytes[idx] {
+            b'\'' => {
+                if within_quotes {
+                    // ie. encountered close quote
+                    sb.drain(..1);
+                    push_str(&mut tokens, &mut sb);
+                } else {
+                    sb.push(bytes[idx] as char);
+                }
+                within_quotes = !within_quotes;
+            }
+            b' ' => {
+                if within_quotes {
+                    sb.push(bytes[idx] as char);
+                } else {
+                    push_str(&mut tokens, &mut sb);
+                }
+            }
+            _ => {
+                sb.push(bytes[idx] as char);
+            }
+        }
+        idx += 1;
+    }
+    if !sb.is_empty() {
+        tokens.push(sb);
+    }
+    tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_command() {
+        let command = "echo 'Hello   world!'";
+        let tokens = parse_command(command.to_string());
+        assert_eq!(tokens, vec!["echo", "'Hello   world!'"]);
+    }
+}
